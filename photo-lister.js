27b9c9ed2658f93f1f24d36b2e765f1b2c2d3c44
glenr4 <glenrutherford4@gmail.com>
2016-07-08 13:09:36 +1000

    added photoListToHTML

--- photo-lister.js
+++ photo-lister.js
@@ -11,7 +11,11 @@ PhotoLister = {
             photo.title,
             '</figcaption></figure></li>'
         ].join('');
-    }
+    },
+
+    photoListToHTML: function(photos) {
+	    return '<ul>' + photos.map(PhotoLister.photoToListItem).join('') + '</ul>';
+	}
 };
 
 module.exports = PhotoLister;
